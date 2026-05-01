6a7cb5895403bf4f31606fcffb6e25cd4eba221d
dotseks <176202616+dotseks@users.noreply.github.com>
2026-05-01 17:01:44 +0300

    Update isOffline check in AccountsManager
    Set isOffline to false in refreshCurrentAccountState method.

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/game/account/AccountsManager.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/game/account/AccountsManager.kt
@@ -211,7 +211,7 @@ object AccountsManager {
      */
     private fun refreshCurrentAccountState() {
         val currentAccount = getCurrentAccount()
-        val isOffline = checkLimit()
+        val isOffline = false
         _currentAccountFlow.update {
             //若处于非正版状态，不允许使用账号
             if (isOffline) null else currentAccount
